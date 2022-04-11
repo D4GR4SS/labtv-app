69bbefb2338a196d62e8d9923577f9e3d344b64e
Davide Grassitelli <dagrassitelli@gmail.com>
2022-04-11 14:04:37 +0200

    commit inizio

---
+++ src/app/components/slider/slider.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Movie } from 'src/app/models/movie';
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss']
+})
+export class SliderComponent implements OnInit {
+
+  @Input() items: Movie[] = [];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { MoviesService } from '../../services/movies.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+
+  movies: any = [];
+
+  constructor(private movieService : MoviesService) { }
+
+  ngOnInit(): void {
+    this.movieService.getMovies().subscribe((response: any)=>{
+      this.movies = response.results;
+      console.log(this.movies);
+    })
+  }
+
+}
